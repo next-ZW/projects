9ac29328d1d920ba36da9fbf437dd8fe7ee15e5d
next-ZW <99071376@mydavinci.nl>
2023-06-14 10:53:10 +0200

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,33 @@
 let lijst = [
-    'images/garnaal.png',
-    'images/kreeft.png',
-    'images/kwal.png',
-    'images/narwal.png',
-    'images/schildpad.png',
-    'images/ster.png',
-    'images/whale.png',
-    'images/zeepaard.png'
+    'garnaal',
+    'kreeft',
+    'kwal',
+    'narwal',
+    'schildpad',
+    'ster',
+    'whale',
+    'zeepaard'
   ];
+ 
   
-  // Function to shuffle the list of images
+function hadleclick(e){
+  e.src = "images/" + e.afbeelding + ".png";
+  console.dir(e)
+}
+
+
+for(x = 0; x < lijst.length; x++){
+  console.log(lijst[x]);
+
+  let plaatje = document.createElement('images');
+  plaatje.src = "images/grey.png";
+  plaatje.setAttribute("onClick","handleClick(this)")
+  plaatje.afbeelding = lijst[x]
+  .appendChild(plaatje);
+
+};
+
+
   function shuffle(array) {
     let currentIndex = array.length, randomIndex;
   
@@ -22,70 +40,3 @@ let lijst = [
   
     return array;
   }
-  
-  // Function to create the memory game board
-  function createMemoryGame() {
-    const imagesContainer = document.getElementById("images");
-    lijst = shuffle(lijst);
-  
-    // Duplicate the list of images
-    const duplicatedList = lijst.concat(lijst);
-  
-    // Shuffle the duplicated list
-    const shuffledList = shuffle(duplicatedList);
-  
-    // Iterate through the shuffled list of images
-    for (let i = 0; i < shuffledList.length; i++) {
-      const image = document.createElement("img");
-      image.src = shuffledList[i];
-      image.classList.add("card");
-      image.addEventListener("click", handleImageClick);
-  
-      // Create a wrapper element for each image to enable flipping
-      const cardWrapper = document.createElement("div");
-      cardWrapper.classList.add("card-wrapper");
-      cardWrapper.appendChild(image);
-  
-      imagesContainer.appendChild(cardWrapper);
-    }
-  }
-  
-  // Function to handle the image click event
-  function handleImageClick(event) {
-    const clickedImage = event.target;
-  
-    // Check if the clicked image is already revealed or matched
-    if (!clickedImage.classList.contains("matched") && !clickedImage.classList.contains("revealed")) {
-      clickedImage.classList.add("revealed");
-  
-      // Check if two images are revealed for comparison
-      const revealedImages = document.querySelectorAll(".revealed:not(.matched)");
-      if (revealedImages.length === 2) {
-        setTimeout(checkMatch, 1000, revealedImages);
-      }
-    }
-  }
-  
-  // Function to check if the revealed images match
-  function checkMatch(revealedImages) {
-    const [firstImage, secondImage] = revealedImages;
-  
-    if (firstImage.src === secondImage.src) {
-      firstImage.classList.add("matched");
-      secondImage.classList.add("matched");
-    } else {
-      firstImage.classList.remove("revealed");
-      secondImage.classList.remove("revealed");
-    }
-  
-    // Check if all images are matched
-    const matchedImages = document.querySelectorAll(".matched");
-    if (matchedImages.length === lijst.length) {
-      const totalTurns = document.querySelectorAll(".revealed").length / 2;
-      alert(`Congratulations! You have completed the game in ${totalTurns} turns.`);
-    }
-  }
-  
-  // Call the createMemoryGame function when the page finishes loading
-  window.addEventListener("load", createMemoryGame);
-  
