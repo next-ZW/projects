636b7f01f37238596961a5374861a9f98274b000
next-ZW <99071376@mydavinci.nl>
2023-06-16 09:59:01 +0200

    project

--- javascript/javascript/script.js
+++ javascript/javascript/script.js
@@ -1,1 +1,18 @@
-var images = ['garnaal.png', 'kreeft.png', 'kwal.png', 'schildpad.png', 'whale.png','ster.png'];
+var images = ['1.png', '2.png', '3.png', '4.png', '5.png','6.png'];
+var i = 0;
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return img();			 
+}
+
+function previous(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return img();			 
+}
+
+function img(){
+	return document.querySelector('.img').setAttribute('src', "images/"+images[i]);
+}
